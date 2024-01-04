000e263b8cec447701f007975feea124f6376537
RodriArrue <arruerodrigo28@gmail.com>
2024-01-04 19:01:15 -0300

    preentrega 2

--- main.js
+++ main.js
@@ -1,87 +1,75 @@
 
-let EleccionUsuario = prompt("ingrese la plataforma para ver sus planes" )
+let EleccionUsuario = prompt("Ingrese la plataforma para ver sus planes");
 
-let netflix = 'netflix'
+// Información de Netflix
+let netflix = {
+  nombre: 'Netflix',
+  planes: {
+    basico: {
+      precio: 2499,
+      impuestos: 60
+    },
+    estandar: {
+      precio: 4199,
+      impuestos: 60
+    },
+    premium: {
+      precio: 5799,
+      impuestos: 60
+    }
+  }
+};
 
-let basico = 'basico'
+// Información de Spotify
+let spotify = {
+  nombre: 'Spotify',
+  planes: {
+    individual: {
+      precio: 599,
+      impuestos: 60
+    },
+    duo: {
+      precio: 799,
+      impuestos: 60
+    },
+    familiar: {
+      precio: 999,
+      impuestos: 60
+    },
+    estudiantes: {
+      precio: 329,
+      impuestos: 60
+    }
+  }
+};
 
-let estandar = 'estandar'
+// Función para calcular impuestos
+function calcularImpuestos(precio, impuestos) {
+  return (precio * impuestos) / 100;
+}
 
-let premium = 'premium'
+if (EleccionUsuario.toLowerCase() === netflix.nombre.toLowerCase()) {
+  let EleccionPlanNetflix = prompt("Seleccione qué plan quiere calcular: Basico, Estandar o Premium");
 
-if (EleccionUsuario == netflix ){
+  if (netflix.planes.hasOwnProperty(EleccionPlanNetflix.toLowerCase())) {
+    let plan = netflix.planes[EleccionPlanNetflix.toLowerCase()];
+    let ValorImpuestos = calcularImpuestos(plan.precio, plan.impuestos);
+    let ValorFinal = plan.precio + ValorImpuestos;
+    console.log(`El valor final del plan ${EleccionPlanNetflix} en ${netflix.nombre} es: ${ValorFinal}`);
+  } else {
+    console.log("¡El plan ingresado no es válido!");
+  }
+} else if (EleccionUsuario.toLowerCase() === spotify.nombre.toLowerCase()) {
+  let EleccionPlanSpotify = prompt("Seleccione qué plan quiere calcular: Individual, Duo, Familiar o Estudiantes");
 
-    let EleccionPlanNetflix = prompt("seleccione que plan quiere calcular : Basico , Estandar o premium")
-
-    if (EleccionPlanNetflix == basico){
-
-        let ValorImpuestosBasico = ((2499 * 155)/100)
-
-        let ValorFinalBasico = 2499 + ValorImpuestosBasico
-        
-        console.log("el valor final del plan Basico es" , ValorFinalBasico)
-    } else if (EleccionPlanNetflix == estandar) {
-
-            let ValorImpuestosEstandar = ((4199 * 155)/100)
-
-            let ValorFinalEstandar = 4199 + ValorImpuestosEstandar
-        
-            console.log("el valor final del plan Estandar es" , ValorFinalEstandar)
-          } else if (EleccionPlanNetflix == premium){
-                
-                let ValorImpuestosPremium = ((5799 * 155)/100)
-
-                let ValorFinalPremium = 5799 + ValorImpuestosPremium
-        
-                console.log("el valor final del plan Premium es" , ValorFinalPremium)
-            } 
-}             
-
-//---------------------------------------------------------------------------------------------------------------------//
-    
-let spotify = 'spotify'
-
-let individual = 'individual'
-
-let duo = 'duo'
-
-let familiar = 'familiar'
-
-let estudiantes = 'estudiantes'
-
-if (EleccionUsuario == spotify){
-
-    let EleccionPlanSpotify = prompt("seleccione que plan quiere calcular : Individual , Duo , Familiar o Estudiantes")
-
-    if (EleccionPlanSpotify == individual){
-
-        ValorImpuestosIndividual = ((599 * 155)/ 100)
-
-        ValorFinalIndividual = 599 + ValorImpuestosIndividual
-
-        console.log("el valor final del plan Indivual es :" , ValorFinalIndividual)
-         } else if (EleccionPlanSpotify == duo){
-
-            let ValorImpuestosDuo = ((799 * 155)/ 100)
-
-            let ValorFinalDuo = 799 + ValorImpuestosDuo
-
-            console.log (" el valor final del plan Duo es :" , ValorFinalDuo)
-            } else if (EleccionPlanSpotify == familiar) {
-
-                let ValorImpuestosFamiliar = ((999 * 155)/ 100)
-
-                let ValorFinalFamiliar = 999 + ValorImpuestosFamiliar
-
-                console.log(" el valor final del plan Familiar es :" , ValorFinalFamiliar)
-                } else if (EleccionPlanSpotify == estudiantes) {
-
-                    let ValorImpuestosEstudiantes = ((329 * 155)/ 100)
-
-                    let ValorFinalEstudiantes = 329 + ValorImpuestosEstudiantes
-
-                    console.log("el valor final del plan Estudiantes es :" , ValorFinalEstudiantes)
-
-                    }
-}   
-
+  if (spotify.planes.hasOwnProperty(EleccionPlanSpotify.toLowerCase())) {
+    let plan = spotify.planes[EleccionPlanSpotify.toLowerCase()];
+    let ValorImpuestos = calcularImpuestos(plan.precio, plan.impuestos);
+    let ValorFinal = plan.precio + ValorImpuestos;
+    console.log(`El valor final del plan ${EleccionPlanSpotify} en ${spotify.nombre} es: ${ValorFinal}`);
+  } else {
+    console.log("¡El plan ingresado no es válido!");
+  }
+} else {
+  console.log("¡La plataforma ingresada no es válida!");
+}
